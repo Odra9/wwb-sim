4fa5d4b21fcf3d45b9e29ee24bd09545c591d4a9
Leonardo <leonardo@fedora.homenet.telecomitalia.it>
2024-08-30 00:25:01 +0200

    FIrst Commit Refactor needed Refine needed Otherwise working TO DO: readme.md

---
+++ wwbsim_v2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int highscore = 0, turnCount=0;
+
+class Player {
+	public:
+	Player(int p) {
+		player = p;
+		place = 0;
+	}
+
+	void roll() {
+		turnCount++;
+		
+		if (rand()%6+1==5) {
+			place++;
+			
+			if (rand()%6+1!=5)
+				place=0; 
+		}
+	
+		if (place>highscore) {
+			highscore = place;
+			cout << "Player " << player << " ha raggiunto la casella " << highscore << " in " << turnCount << " turni\n";
+		}
+	}
+
+	private:
+	int place;
+	int player;
+};
+
+int main() {
+	srand(time(NULL));
+
+	Player player1(1);
+	Player player2(2);
+
+	while(true) {
+		player1.roll();
+		player2.roll();		
+	}
+
+	return 0;
+}
